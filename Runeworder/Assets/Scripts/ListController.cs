4ed82757aecd66456c424cfbeaa2be028ad66f55
V.Larkin <iamhxckid@yandex.ru>
2022-01-16 02:23:38 +0300

    Huge work with UI
    Reforged UI. New UI Prefab, plus some fixes. Runeword UI Controller now works fine. Need some fixes with user runes filter.

---
+++ Runeworder/Assets/Scripts/ListController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListController : MonoBehaviour
+{
+    public Image background;
+    public Text runewordName;
+    public List<Text> runes;
+    public Text reqLevel;
+    public Text type;
+}
--- Runeworder/Assets/Scripts/RunewordsController.cs
+++ Runeworder/Assets/Scripts/RunewordsController.cs
@@ -8,20 +8,49 @@ public class RunewordsController : MonoBehaviour
 {
     public UserRunes_SO userRunes;
     public RunewordsDB_SO runewordsDB;
+    public GameObject runewordPrefab;
+    public GameObject uiParent;
+
+    [SerializeField] List<GameObject> runewordsToShow;
 
     private void Start()
     {
         RuneController.OnRuneToggleChanged += RuneStateChanged;
-        ToggleController.OnToggleChanged += TypeStateChanged;
-    }
-
-    private void TypeStateChanged(Toggle toggle, bool isOn)
-    {
-        
+        runewordsToShow = new List<GameObject>();
+        runewordsToShow.Clear();
+        UpdateRunewordsList();
     }
 
     private void RuneStateChanged(Runes rune, bool isOn)
     {
-        
+        UpdateRunewordsList();
+    }
+
+    private void UpdateRunewordsList()
+    {
+        int odd = 1;
+        foreach (var rw in runewordsDB.runewords)
+        {
+            ListController lc = runewordPrefab.GetComponent<ListController>();
+            if (odd % 2 == 0)
+                lc.background.color = Color.red;
+            else
+                lc.background.color = Color.white;
+            odd++;
+            lc.runewordName.text = rw.runewordName;
+            foreach (var rune in lc.runes)
+            {
+                rune.text = "";
+            }
+            for (int i = 0; i < rw.runes.Count; i++)
+            {
+                lc.runes[i].text = rw.runes[i].ToString();
+            }
+            lc.reqLevel.text = $"Req Lvl: {rw.reqLevel}";
+            lc.type.text = $"{rw.runewordType}";
+
+            GameObject inst = Instantiate(runewordPrefab, uiParent.transform);
+            runewordsToShow.Add(inst);
+        }
     }
 }
