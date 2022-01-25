eeb7eb8fa7733199eb202b41e3d4633666a3087a
V.Larkin <iamhxckid@yandex.ru>
2022-01-26 00:27:54 +0300

    UI
    QuitText fix
    Banner try fix

--- Runeworder/Assets/Scripts/Ads/BannerAd.cs
+++ Runeworder/Assets/Scripts/Ads/BannerAd.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
@@ -7,10 +8,18 @@ public class BannerAd : MonoBehaviour
     [SerializeField] BannerPosition bannerPosition;
 
     [SerializeField] string androidAdUnitId = "Banner_Android";
+    [SerializeField] float bannerLoadDelay = 2f;
 
     private void Start()
     {
         Advertisement.Banner.SetPosition(bannerPosition);
+        StartCoroutine(LoadBannerAfterTime(bannerLoadDelay));
+        //LoadBanner();
+    }
+
+    private IEnumerator LoadBannerAfterTime(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         LoadBanner();
     }
 
@@ -45,19 +54,23 @@ public class BannerAd : MonoBehaviour
             showCallback = OnBannerShown
         };
 
+        Debug.Log("Banner Show");
         Advertisement.Banner.Show(androidAdUnitId, options);
     }
 
     private void OnBannerClicked()
     {
+        Debug.Log("Banner Clicked");
     }
 
     private void OnBannerHidden()
     {
+        Debug.Log("Banner Hidden");
     }
 
     private void OnBannerShown()
     {
+        Debug.Log("Banner Shown");
     }
 
     public void HideBannerAd()
