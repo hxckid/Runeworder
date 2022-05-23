50e54dd39e3bf3c4aac2d268d09d3f3c17add954
V.Larkin <iamhxckid@yandex.ru>
2022-05-23 17:04:47 +0300

    Buttons Localization

--- Runeworder/Assets/Scripts/AppManager.cs
+++ Runeworder/Assets/Scripts/AppManager.cs
@@ -22,8 +22,7 @@ public class AppManager : MonoBehaviour
     public GameObject runesBtn;
     public GameObject runewordsBtn;
     public Languages currentLanguage;
-    public Canvas canvas;
-
+    public List<Text> buttonsText;
     public Font enFont;
     public Font ruFont;
     Text[] links;
@@ -33,30 +32,79 @@ public class AppManager : MonoBehaviour
     string key = "UserData";
     GameObject txt;
 
+    
+
     public delegate void LanguageHandler(Languages languages);
     public static event LanguageHandler OnLanguageChanged;
 
     private void OnValidate()
     {
-        //links = canvas.GetComponentsInChildren<Text>();
-
         switch (currentLanguage)
         {
             case Languages.En:
                 OnLanguageChanged?.Invoke(Languages.En);
-                //foreach (var text in links)
-                //{
-                //    text.font = enFont;
-                //}
+                foreach (var button in buttonsText)
+                {
+                    button.font = enFont;
+                }
+                buttonsText[0].fontSize = 80;
+                buttonsText[0].text = "Reset";
+                buttonsText[1].fontSize = 70;
+                buttonsText[1].text = "Show Runewords";
+                buttonsText[2].fontSize = 70;
+                buttonsText[2].text = "Back to Runes";
+                buttonsText[3].fontSize = 58;
+                buttonsText[3].text = "All";
+                buttonsText[4].fontSize = 58;
+                buttonsText[4].text = "Weapons";
+                buttonsText[5].fontSize = 58;
+                buttonsText[5].text = "Armors";
+                buttonsText[6].fontSize = 58;
+                buttonsText[6].text = "Helms";
+                buttonsText[7].fontSize = 58;
+                buttonsText[7].text = "Shields";
+                buttonsText[8].fontSize = 48;
+                buttonsText[8].text = "Patch 2.4";
+                buttonsText[9].fontSize = 62;
+                buttonsText[9].text = "Name";
+                buttonsText[10].fontSize = 62;
+                buttonsText[10].text = "Runes";
+                buttonsText[11].fontSize = 62;
+                buttonsText[11].text = "Level";
                 break;
             case Languages.Ru:
                 OnLanguageChanged?.Invoke(Languages.Ru);
-                //foreach (var text in links)
-                //{
-                //    text.font = ruFont;
-                //}
+                foreach (var button in buttonsText)
+                {
+                    button.font = ruFont;
+                }
+                buttonsText[0].fontSize = 64;
+                buttonsText[0].text = "Сбросить";
+                buttonsText[1].fontSize = 56;
+                buttonsText[1].text = "К рунвордам";
+                buttonsText[2].fontSize = 52;
+                buttonsText[2].text = "Назад к рунам";
+                buttonsText[3].fontSize = 46;
+                buttonsText[3].text = "Все";
+                buttonsText[4].fontSize = 46;
+                buttonsText[4].text = "Оружие";
+                buttonsText[5].fontSize = 46;
+                buttonsText[5].text = "Броня";
+                buttonsText[6].fontSize = 46;
+                buttonsText[6].text = "Шлемы";
+                buttonsText[7].fontSize = 46;
+                buttonsText[7].text = "Щиты";
+                buttonsText[8].fontSize = 40;
+                buttonsText[8].text = "Патч 2.4";
+                buttonsText[9].fontSize = 46;
+                buttonsText[9].text = "Имя";
+                buttonsText[10].fontSize = 46;
+                buttonsText[10].text = "Руны";
+                buttonsText[11].fontSize = 46;
+                buttonsText[11].text = "Уровень";
                 break;
         }
+        
     }
 
     private void OnEnable()
